edb1cce20fd4860ba489722fb3254d289b9ccb32
Guilherme Bitencourt <guilherme.bitencourt@pravaler.com.br>
2024-10-01 16:50:28 -0300

    fix: build

--- src/layouts/contexts/auth/user-context.ts
+++ src/layouts/contexts/auth/user-context.ts
@@ -1,14 +1,13 @@
-import type * as React from 'react';
+import type * as React from "react";
 
-import { config } from '@/config';
-import { AuthStrategy } from '@/lib/auth/strategy';
+import { config } from "@/config";
+import { AuthStrategy } from "@/lib/auth/strategy";
 
-import { UserContext as Auth0UserContext, UserProvider as Auth0UserProvider } from './auth0/user-context';
-import { UserContext as CognitoUserContext, UserProvider as CognitoUserProvider } from './cognito/user-context';
-import { UserContext as CustomUserContext, UserProvider as CustomUserProvider } from './custom/user-context';
-import { UserContext as FirebaseUserContext, UserProvider as FirebaseUserProvider } from './firebase/user-context';
-import { UserContext as SupabaseUserContext, UserProvider as SupabaseUserProvider } from './supabase/user-context';
-import type { UserContextValue } from './types';
+import {
+  UserContext as FirebaseUserContext,
+  UserProvider as FirebaseUserProvider,
+} from "./firebase/user-context";
+import type { UserContextValue } from "./types";
 
 // eslint-disable-next-line import/no-mutable-exports -- Export based on config
 let UserProvider: React.FC<{ children: React.ReactNode }>;
@@ -38,7 +37,7 @@ switch (config.auth.strategy) {
   //   UserProvider = SupabaseUserProvider;
   //   break;
   default:
-    throw new Error('Invalid auth strategy');
+    throw new Error("Invalid auth strategy");
 }
 
 export { UserProvider, UserContext };
